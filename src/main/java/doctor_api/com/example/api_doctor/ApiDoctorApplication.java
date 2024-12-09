2552989b7d4b8504c8b4ef1e1ccc0bca69535f5a
Asepimam <asepimam329@gmail.com>
2024-12-09 21:14:43 +0700

    initial

---
+++ src/main/java/doctor_api/com/example/api_doctor/ApiDoctorApplication.java
@@ -0,0 +1,13 @@
+package doctor_api.com.example.api_doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/doctor_api/com/example/api_doctor/ApiDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package doctor_api.com.example.api_doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
