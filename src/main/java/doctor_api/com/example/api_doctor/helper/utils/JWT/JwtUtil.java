1a338c48b9b7aa0d4d4cbc1a1a2d4cdb138150bd
Asepimam <asepimam329@gmail.com>
2024-12-23 00:13:28 +0700

    feat: implement JWT authentication with JwtRequestFilter and JwtUtil classes

---
+++ src/main/java/doctor_api/com/example/api_doctor/helper/utils/JWT/JwtUtil.java
@@ -0,0 +1,41 @@
+package doctor_api.com.example.api_doctor.helper.utils.JWT;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private  String SECRET_KEY;
+    @Value("${jwt.issuer}")
+    private  String ISSUER;
+
+    public String genereteToken(String username){
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer(ISSUER)
+                .withExpiresAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.HMAC256(SECRET_KEY));
+    }
+
+    public String validateToken(String token){
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET_KEY))
+                    .withIssuer(ISSUER)
+                    .build();
+
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT.getSubject();
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid token");
+        }
+    }
+}
