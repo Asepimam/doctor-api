af62540ce8bdc74536deed755e59708b3e1be365
Asepimam <asepimam329@gmail.com>
2024-12-23 23:36:55 +0700

    feat: add DoctorController for managing doctor-related endpoints

---
+++ src/main/java/doctor_api/com/example/api_doctor/controller/DoctorController.java
@@ -0,0 +1,10 @@
+package doctor_api.com.example.api_doctor.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+    
+}
